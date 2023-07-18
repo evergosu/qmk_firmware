201fef58326e017c27de8583c2e7483ec26b6b3a
Evergosu <evergosu@gmail.com>
2023-07-18 16:08:46 +0300

    Created custom keymap from default with apple fn support.

--- keyboards/lets_split/rules.mk
+++ keyboards/lets_split/rules.mk
@@ -7,9 +7,10 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+APPLE_FN_ENABLE = yes       # Enable Apple Fn key functionality
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 
 SPLIT_KEYBOARD = yes
 
